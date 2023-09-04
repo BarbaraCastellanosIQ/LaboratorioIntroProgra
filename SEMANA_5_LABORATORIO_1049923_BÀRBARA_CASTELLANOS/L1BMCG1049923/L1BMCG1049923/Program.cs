00bb2c11f60ee1745dbe58098bdc45a9119f752b
BarbaraCastellanosIQ <141659017+BarbaraCastellanosIQ@users.noreply.github.com>
2023-09-04 10:08:55 -0600

    Add files via upload

---
+++ SEMANA_5_LABORATORIO_1049923_BÀRBARA_CASTELLANOS/L1BMCG1049923/L1BMCG1049923/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+        Console.WriteLine("Hola Mundo");
+        Console.WriteLine("soy Bárbara");
+
+        //Console.Write("Hola Mundo");
+        //Console.Write(" soy Bárbara");*/
+
+        ///*comentarios programa*/
+        //Console.ReadKey();
+        //Console.ReadLine();*/
+
+ 
+
+    Console.WriteLine("Ingrese su nombre:");
+    string Nombre = Console.ReadLine();
+
+    Console.WriteLine("Hola Mundo");
+    Console.WriteLine("soy " + Nombre);
+
+    Console.Write("Hola Mundo");
+    Console.Write(" soy " + Nombre);
+    Console.ReadKey();
+    }
+}
