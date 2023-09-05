7079789064cd59249424f25e31d5a3ff5c609dc2
BarbaraCastellanosIQ <141659017+BarbaraCastellanosIQ@users.noreply.github.com>
2023-09-04 21:32:49 -0600

    Add files via upload

---
+++ SEMANA_5_LABORATORIO_1049923_BÀRBARA_CASTELLANOS/L1BMCG1049923/T1_BMCG_1049923/T1_BMCG_1049923/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    /*ENTRADA*/
+
+        Console.WriteLine("Mi segundo programa");
+        /*Nombre*/
+    
+        Console.WriteLine("Ingrese su nombre:");
+        string Nombre = Console.ReadLine();
+
+        /*Edad*/
+        Console.WriteLine("Ingrese su edad:");
+        string Edad = Console.ReadLine();
+
+        /*Carrera*/
+        Console.WriteLine("Ingrese su carrera:");
+        string Carrera = Console.ReadLine();
+
+        /*Carné*/
+        Console.WriteLine("Ingrese su carné:");
+        string Carné = Console.ReadLine();
+
+    /*SALIDA*/
+        Console.WriteLine("Mi segundo programa");
+        Console.WriteLine("Nombre:" + Nombre);
+        Console.WriteLine("Edad:" + Edad);
+        Console.WriteLine("Carrera:" + Carrera);
+        Console.WriteLine("Carné:" + Carné);
+        Console.WriteLine("Soy " + Nombre + "," + " tengo " + Edad + " años " + " y estudio la carrera de " + Carrera + ".");
+        Console.WriteLine("Mi número de Carné es: " + Carné);
+
+        Console.ReadKey();
+    }
+}
