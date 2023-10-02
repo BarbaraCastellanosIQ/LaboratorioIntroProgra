df6f626aa5fef8c40203bbf463928af77866b140
BarbaraCastellanosIQ <141659017+BarbaraCastellanosIQ@users.noreply.github.com>
2023-10-02 10:06:54 -0600

    Add files via upload

---
+++ SEMANA_9_LAB_1049923_BÁRBARA_CASTELLANOS/L9_EJERCICIO1_1049923_BMCG/L9_1049923_BMCG/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese un número entero mayor que 0");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int contador = 1;
+        int sumatoria = 0;
+        string texto = "";
+
+        while (contador <= n)
+        {
+            texto = texto + contador + "+";
+            sumatoria = sumatoria + contador;
+            contador++;
+
+        }
+
+        Console.WriteLine(texto);
+
+    }
+}
---
+++ SEMANA_9_LAB_1049923_BÁRBARA_CASTELLANOS/L9_EJERCICIO2_1049923_BMCG/L9_EJERCICIO2_1049923_BMCG/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ejercicio No. 2");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.Write("\nTabla del " + i + ":\t");
+            for (int j = 1; j <= n; j++)
+            {
+                Console.Write(i * j + "\t");
+            }
+        }
+        Console.ReadLine();
+    }
+}
+
