934283e4350f86f5892879e9a89959e29aeb3102
BarbaraCastellanosIQ <141659017+BarbaraCastellanosIQ@users.noreply.github.com>
2023-10-09 10:04:32 -0600

    Add files via upload

---
+++ SEMANA_10_LAB_1019923_BMCG/L10_1049923_BMCG/L10_1049923_BMCG/Program.cs
@@ -0,0 +1,38 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        int numero;
+        Estudiante estudiante_1 = new Estudiante("Bárbara Castellanos", 18, "Zona 10 de Mixco");
+
+        Console.WriteLine("Nombre: " + estudiante_1.nombre);
+        Console.WriteLine("Edad: " + estudiante_1.edad);
+        Console.WriteLine("Dirección: " + estudiante_1.direccion);
+
+        Console.ReadKey();
+    }
+
+
+    public class Estudiante
+    {
+        public string nombre { get; set; }
+
+        public int edad { get; set; }
+
+        public string direccion { get; set; }
+
+        public Estudiante()
+        {
+            this.nombre = "Bárbara Castellanos";
+            this.edad = 18;
+            this.direccion = "Zona 10 de Mixco";
+        }
+
+        public Estudiante(string nombre, int edad, string direccion)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.direccion = direccion;
+        }
+    }
+}
